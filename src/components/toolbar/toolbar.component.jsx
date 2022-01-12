b1824d8828403abf3e475817c3a30a20653b26c6
JamesGabby <jlghfe36@gmail.com>
2022-01-12 06:20:48 +0000

    Toolbar styling

--- src/components/toolbar/toolbar.component.jsx
+++ src/components/toolbar/toolbar.component.jsx
@@ -28,20 +28,20 @@ const Toolbar = () => {
                 </div>
             </div>
             <div className={count ? 'List active' : 'List'}>
-                    <div>
+                    <div className="List-child-top">
                         <a style={{color: '#fff'}} href="/#home" onClick={() => setCount(!count)}><h2>Home</h2></a>
                     </div>
-                    <div>
+                    <div className="List-child">
                         <a title="CV" style={{color: '#fff'}} target="_blank" rel="noopener noreferrer" href="https://docs.google.com/document/d/1XF3AFJrVP6b76BS4pD-xg2huL65WEDnNCm9g5Zq2skE/edit?usp=sharing"><h2>CV</h2></a>
                     </div>
-                    <div>
+                    <div className="List-child">
                         <a title="Photography">
                             <Link to='/photos'>
                                 <h2>Photography</h2>
                             </Link>
                         </a>            
                     </div>
-                    <div>
+                    <div className="List-child">
                         <a style={{color: '#fff'}} href="/#contact" onClick={() => setCount(!count)}><h2>Contact</h2></a>
                     </div>
                 </div>
